535499fda12f307de852bf3239ad4a356a0d285e
Bartosz Gajda <bg@bartoszgajda.com>
2019-07-27 21:07:22 +0200

    Add Kafka Producer and update Socket Handler.

--- collection/src/main/java/bg/bartoszgajda/collection/CollectionSocketHandler.java
+++ collection/src/main/java/bg/bartoszgajda/collection/CollectionSocketHandler.java
@@ -5,10 +5,21 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Component
 public class CollectionSocketHandler extends AbstractWebSocketHandler {
+  private static final Logger logger = Logger.getLogger(CollectionSocketHandler.class.getName());
+  private final KafkaProducer kafkaProducer;
+
+  public CollectionSocketHandler(KafkaProducer kafkaProducer) {
+    this.kafkaProducer = kafkaProducer;
+  }
+
   @Override
   public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-    System.out.println(message.getPayload());
+    logger.log(Level.INFO, "New RSVP:\n {0}", message.getPayload());
+    kafkaProducer.sendMessage(message);
   }
 }
