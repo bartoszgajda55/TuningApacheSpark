ccc78b56b73bb23803c3fe22943f6f94b7bd8815
Bartosz Gajda <bg@bartoszgajda.com>
2019-07-27 15:14:07 +0200

    Add WS handler code to collection tier.

--- collection/src/main/java/bg/bartoszgajda/collection/CollectionApplication.java
+++ collection/src/main/java/bg/bartoszgajda/collection/CollectionApplication.java
@@ -1,13 +1,26 @@
 package bg.bartoszgajda.collection;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 @SpringBootApplication
 public class CollectionApplication {
+	private static final String MEETUP_RSVP_ENDPOINT = "ws://stream.meetup.com/2/rsvps";
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollectionApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner initializeConnection(CollectionSocketHandler collectionSocketHandler) {
+		return args -> {
+			WebSocketClient socketClient = new StandardWebSocketClient();
+			socketClient.doHandshake(collectionSocketHandler, MEETUP_RSVP_ENDPOINT);
+		};
 	}
 
 }
