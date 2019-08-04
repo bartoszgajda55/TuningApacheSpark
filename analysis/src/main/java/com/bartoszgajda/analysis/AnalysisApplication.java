0ba8197bf0ac1932bed8a82f8389d6cbd35a6086
Bartosz Gajda <bg@bartoszgajda.com>
2019-08-04 20:01:59 +0200

    Add Kafka and Mongo config to Analysis tier

--- analysis/src/main/java/com/bartoszgajda/analysis/AnalysisApplication.java
+++ analysis/src/main/java/com/bartoszgajda/analysis/AnalysisApplication.java
@@ -1,7 +1,13 @@
 package com.bartoszgajda.analysis;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AnalysisApplication {
 
@@ -9,13 +15,35 @@ public class AnalysisApplication {
   private static final String HADOOP_HOME_DIR_VALUE = "C:/winutils";
   private static final String RUN_LOCAL_WITH_AVAILABLE_CORES = "local[*]";
 
+  private static final Map<String, Object> KAFKA_CONSUMER_PROPERTIES;
+
+  private static final String KAFKA_BROKERS = "localhost:9092";
+  private static final String KAFKA_OFFSET_RESET_TYPE = "latest";
+  private static final String KAFKA_GROUP = "meetupGroup";
+  private static final String KAFKA_TOPIC = "meetupTopic";
+
+  static {
+    Map<String, Object> kafkaProperties = new HashMap<>();
+    kafkaProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
+    kafkaProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    kafkaProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    kafkaProperties.put(ConsumerConfig.GROUP_ID_CONFIG, KAFKA_GROUP);
+    kafkaProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, KAFKA_OFFSET_RESET_TYPE);
+    kafkaProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+
+    KAFKA_CONSUMER_PROPERTIES = Collections.unmodifiableMap(kafkaProperties);
+  }
+
+  private static final String MONGODB_OUTPUT_URI = "mongodb://localhost/meetupDB.rsvps";
+
   public static void main(String[] args) throws InterruptedException {
 
     System.setProperty("hadoop.home.dir", HADOOP_HOME_DIR_VALUE);
 
     final SparkConf conf = new SparkConf()
+      .setMaster(RUN_LOCAL_WITH_AVAILABLE_CORES)
       .setAppName(APPLICATION_NAME)
-      .setMaster(RUN_LOCAL_WITH_AVAILABLE_CORES);
+      .set("spark.mongodb.output.uri", MONGODB_OUTPUT_URI);
 
     JavaSparkContext sparkContext = new JavaSparkContext(conf);
 
