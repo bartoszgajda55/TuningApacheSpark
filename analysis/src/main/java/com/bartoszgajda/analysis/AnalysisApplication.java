1920654bd801a3e7c9911332bed0170580a555ea
Bartosz Gajda <bg@bartoszgajda.com>
2019-08-20 11:47:20 +0200

    Save RDDs to Mongo.

--- analysis/src/main/java/com/bartoszgajda/analysis/AnalysisApplication.java
+++ analysis/src/main/java/com/bartoszgajda/analysis/AnalysisApplication.java
@@ -1,9 +1,16 @@
 package com.bartoszgajda.analysis;
 
+import com.mongodb.spark.MongoSpark;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.apache.spark.streaming.kafka010.OffsetRange;
+import org.bson.Document;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -11,16 +18,18 @@ import java.util.Map;
 
 public class AnalysisApplication {
 
-  private static final String APPLICATION_NAME = "Analysis Tier";
+  private static final String APPLICATION_NAME = "Spark RDD";
   private static final String HADOOP_HOME_DIR_VALUE = "C:/winutils";
   private static final String RUN_LOCAL_WITH_AVAILABLE_CORES = "local[*]";
 
   private static final Map<String, Object> KAFKA_CONSUMER_PROPERTIES;
 
   private static final String KAFKA_BROKERS = "localhost:9092";
-  private static final String KAFKA_OFFSET_RESET_TYPE = "latest";
   private static final String KAFKA_GROUP = "meetupGroup";
   private static final String KAFKA_TOPIC = "meetupTopic";
+  private static final OffsetRange[] offsetRange =
+    // topic, partition, inclusive starting offset, exclusive ending offset
+    { OffsetRange.create(KAFKA_TOPIC, 0, 1, 100) };
 
   static {
     Map<String, Object> kafkaProperties = new HashMap<>();
@@ -28,8 +37,6 @@ public class AnalysisApplication {
     kafkaProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
     kafkaProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
     kafkaProperties.put(ConsumerConfig.GROUP_ID_CONFIG, KAFKA_GROUP);
-    kafkaProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, KAFKA_OFFSET_RESET_TYPE);
-    kafkaProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
 
     KAFKA_CONSUMER_PROPERTIES = Collections.unmodifiableMap(kafkaProperties);
   }
@@ -47,7 +54,11 @@ public class AnalysisApplication {
 
     JavaSparkContext sparkContext = new JavaSparkContext(conf);
 
-    // business logic
+    JavaRDD<ConsumerRecord<String, String>> rdd =
+      KafkaUtils.createRDD(sparkContext, KAFKA_CONSUMER_PROPERTIES,
+        offsetRange, LocationStrategies.PreferConsistent());
+
+    MongoSpark.save(rdd.map(r -> Document.parse(r.value())));
 
     sparkContext.stop();
     sparkContext.close();
